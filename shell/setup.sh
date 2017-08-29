6155b4ac9fe2721639456e882c0ced37ed2ce95c
Lindsay Holmwood <lindsay@holmwood.id.au>
2017-08-29 22:50:36 +1000

    Attempt to fix OpenSSL, because Ansible fails with SSL_ST_INIT

--- shell/setup.sh
+++ shell/setup.sh
@@ -3,4 +3,11 @@
 apt update
 apt install -y python-setuptools build-essential python-dev
 easy_install pip
+
+# Try to fix OpenSSL
+rm -rf /usr/lib/python2.7/dist-packages/OpenSSL
+rm -rf /usr/lib/python2.7/dist-packages/pyOpenSSL-0.15.1.egg-info
+pip install pyopenssl
+
+# Then install Ansible
 pip install ansible
